2473f512c4e5901c662d9bda2a152e3566b2025f
victor <vhenrixon@gmail.com>
2019-12-24 12:28:46 -0500

    Helmet install and http

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+var https = require('https')
+var http = require('http')
+var helmet = require('helmet');
 
 //  Require Passport
 var passport = require('passport');
@@ -16,7 +17,6 @@ require('./api/models/database');
 require('./api/config/passport');
 require('dotenv').config();
 
-
 //  Bring in the routes for the API (delete the default routes)
 var authRoutes = require('./api/routes/authRoutes');
 var eventRoutes = require("./api/routes/eventRoutes");
@@ -26,14 +26,22 @@ var progressRoutes = require("./api/routes/progressRoute");
 
 var app = express();
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
-app.use(cors());
-
+app.use(cors({
+	origin: "*",
+    optionsSuccessStatus: 200
+}));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 //  Initialise Passport before using the route middleware
 app.use(passport.initialize());
 
@@ -87,7 +95,19 @@ app.use(function (err, req, res, next) {
 if (process.env.NODE_ENV == "development") {
     app.listen(process.env.NODE_PORT, () => console.log("Epxress local server running"));
 } else {
+    http.createServer(app).listen(80, () => {
+        console.log('[√] Http is up')
+    })
+
+    https.createServer({
+        key: fs.readFileSync('/etc/letsencrypt/live/api.comprehendcode.org/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/api.comprehendcode.org/fullchain.pem'),
+        ca: fs.readFileSync('/etc/letsencrypt/live/api.comprehendcode.org/fullchain.pem')
+    }, app).listen(443, () => {
+        console.log('[√] Https is up')
+    })
     console.log("Express server running for production");
+
 }
 
 
