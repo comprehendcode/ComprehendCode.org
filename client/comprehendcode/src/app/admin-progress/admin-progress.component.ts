66e0d11a8579a827fe6318e58cb4a87d86fbccb1
Chip <chipgad@gmail.com>
2019-12-24 22:35:47 -0500

    Our-progress Admin
    Admin side of our Progress

--- client/comprehendcode/src/app/admin-progress/admin-progress.component.ts
+++ client/comprehendcode/src/app/admin-progress/admin-progress.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-admin-progress',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProgressComponent implements OnInit {
 
-  constructor() { }
+  constructor(private post: PostService) { }
 
   ngOnInit() {
+    this.post
   }
 
+  addPost(){
+    return PostService.addPost(this.post).subscribe();
+  }
 }
